d83e2058824942c2583a1173f4bed7ddc2e9e721
Matthew Erhard <merhard@gmail.com>
2013-06-12 16:12:44 -0400

    initial commit
    postgresql, devise, pry, rspec, capybara, factory-girl, and launchy gems bundled
    db created and migrated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toy::Application.config.secret_token = '1b7609206773e13c7376f9abffc4dc5daf6511ddf8eed026e3209b43deaa556de209a6bdbd3e3429f77d23a3353be171650e8fa68dd4793b93cac9891bfe4145'
