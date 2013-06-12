83736395b03b21ee61eaf576ba94f02f6194160b
Matthew Erhard <merhard@gmail.com>
2013-06-12 18:02:39 -0400

    find name on root page add css reset add initial wireframe css add columns css

--- spec/features/user_visits_root_page_spec.rb
+++ spec/features/user_visits_root_page_spec.rb
@@ -5,4 +5,9 @@ describe 'visiting the root page' do
     visit '/'
     expect(page.status_code).to eq(200)
   end
+
+  it 'displays app name' do
+    visit '/'
+    expect(page).to have_content('Sport Finder')
+  end
 end
