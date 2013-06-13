caaa7769bfd7f5d508bc54d2a39d96dc34643c09
Matthew Erhard <merhard@gmail.com>
2013-06-13 15:47:58 -0400

    add link for each sport on search page

--- spec/features/user_visits_search_page_spec.rb
+++ spec/features/user_visits_search_page_spec.rb
@@ -1,20 +1,22 @@
 require 'spec_helper'
 
 describe 'visiting the search page' do
+  let!(:sport) { FactoryGirl.create(:sport) }
+
   it 'exists' do
     visit '/search'
     expect(page.status_code).to eq(200)
   end
 
   it 'shows list of sports to search by' do
-    sport = FactoryGirl.create(:sport)
-
     visit '/search'
-
     expect(page).to have_content(sport.name)
   end
 
-
+  it 'has a link for each sport' do
+    visit '/search'
+    expect(page).to have_selector('a', text: sport.name)
+  end
 
 
 end
