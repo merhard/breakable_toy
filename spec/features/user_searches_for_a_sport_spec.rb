816fba8aa8647c6a82dcd12a734122be2bc4f60c
Matthew Erhard <merhard@gmail.com>
2013-06-14 20:22:35 -0400

    list court locations on sport search page

--- spec/features/user_searches_for_a_sport_spec.rb
+++ spec/features/user_searches_for_a_sport_spec.rb
@@ -8,18 +8,20 @@ describe 'searching for a sport' do
     court.save
     @sport_with_courts = Court.last.sport
   end
+  before(:each) { visit sport_courts_path(sport_with_courts) }
 
   it 'displays an added court' do
-    visit sport_courts_path(sport_with_courts)
     expect(page).to have_content(sport_with_courts.courts[0].number_of)
   end
 
   it 'displays courts on sport search page' do
-    visit sport_path(sport_with_courts)
     expect(page).to have_content(sport_with_courts.courts[0].number_of)
   end
 
-
+  it 'displays court locations' do
+    location = sport_with_courts.courts.last.location
+    expect(page).to have_content(location.street_address)
+  end
 
 
 end
