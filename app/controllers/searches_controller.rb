f99362e9ae29f55d2748ad96be3d9ad1bed00dd9
Matthew Erhard <merhard@gmail.com>
2013-06-27 12:04:23 -0400

    can sort search result columns by asc or desc order

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,7 @@
 class SearchesController < ApplicationController
+  helper_method :sort_column, :sort_direction
   def show
     @q = Court.search(params[:q])
-    @courts = @q.result(distinct: true)
+    @courts = @q.result
   end
 end
