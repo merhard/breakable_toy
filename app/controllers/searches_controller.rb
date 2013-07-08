2f6ed0999d22e0750cdd81dc16a70368ac631186
Matthew Erhard <merhard@gmail.com>
2013-07-08 17:29:55 -0400

    add distance searching
    - search within a given radius
    - set geocoding to be done by geocoder instead of gmaps4rails
    - only show radius search to signed in users
    - remove secret token from files
    - changed secret token

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,13 +2,47 @@ class SearchesController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def show
-    unless params[:q]
+    if user_signed_in?
+      @location = current_user.location
+    end
+
+    if params[:q]
+
+      if params[:within] && params[:within] != ''
+
+        locations = Location.near(@location, params[:within])
+        locations_id_array = []
+        locations.each do |location|
+          locations_id_array << location.id
+        end
+
+        if params[:q][:s]
+          @q = Court.where(location_id: locations_id_array).limit(200).search(params[:q])
+          court_results = @q.result.all.uniq
+          @courts = Kaminari.paginate_array(court_results).page(params[:page]).per(5)
+        else
+          @q = Court.where(location_id: locations_id_array).search(params[:q])
+          @courts = @q.result(distinct: true).page(params[:page]).per(5)
+        end
+
+      else
+
+        if params[:q][:s]
+          @q = Court.limit(200).search(params[:q])
+          court_results = @q.result.all.uniq
+          @courts = Kaminari.paginate_array(court_results).page(params[:page]).per(5)
+        else
+          @q = Court.search(params[:q])
+          @courts = @q.result(distinct: true).page(params[:page]).per(5)
+        end
+
+      end
+
+    else
+
       @q = Court.search(params[:q])
       @courts = Court.page(params[:page]).per(5)
-    else
-      @q = Court.limit(200).search(params[:q])
-      court_results = @q.result.all.uniq
-      @courts = Kaminari.paginate_array(court_results).page(params[:page]).per(5)
+
     end
 
     @locations = []
@@ -17,7 +51,6 @@ class SearchesController < ApplicationController
     end
 
     if user_signed_in?
-      @location = current_user.location
       @locations << @location
     end
 
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Toy::Application.config.secret_token = '1b7609206773e13c7376f9abffc4dc5daf6511ddf8eed026e3209b43deaa556de209a6bdbd3e3429f77d23a3353be171650e8fa68dd4793b93cac9891bfe4145'
+Toy::Application.config.secret_token = ENV['SECRET_KEY']
